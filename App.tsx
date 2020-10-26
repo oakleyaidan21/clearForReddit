d1abb0999473e78e3e8dccfadecef8005ac1f092
Aidan Oakley <oaklea@aidans-mbp.home>
2020-10-26 12:20:01 -0400

    fixed splash screens for ios and android

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import ClearForReddit from './ClearForReddit';
 import {store, persistor} from './redux/store';
 import {decode, encode} from 'base-64';
 import {PersistGate} from 'redux-persist/integration/react';
-//import * as SplashScreen from 'expo-splash-screen';
+import SplashScreen from 'react-native-splash-screen';
 
 declare var global: any;
 
@@ -21,9 +21,7 @@ LogBox.ignoreLogs(['Setting a timer']);
 
 export default function App() {
   useEffect(() => {
-    // const splashHideTimer = setTimeout(async () => {
-    //   await SplashScreen.hideAsync();
-    // }, 1000);
+    SplashScreen.hide();
   }, []);
 
   const [clear, setClear] = useState(null);
--- redux/store.tsx
+++ redux/store.tsx
@@ -1,12 +1,12 @@
-import { AsyncStorage } from "react-native";
+import AsyncStorage from '@react-native-community/async-storage';
 
-import { persistStore, persistReducer } from "redux-persist";
+import {persistStore, persistReducer} from 'redux-persist';
 
-import reducer from "./reducer";
-import { createStore } from "redux";
+import reducer from './reducer';
+import {createStore} from 'redux';
 
 const persistConfig = {
-  key: "root",
+  key: 'root',
   storage: AsyncStorage,
 };
 
@@ -16,4 +16,4 @@ const store = createStore(persistingReducer);
 
 const persistor = persistStore(store);
 
-export { store, persistor };
+export {store, persistor};
